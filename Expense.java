c1d1c0fd81a2765d1a4cf9de72bfe247c185b38a
sweatha303-create <sweatha303@gmail.com>
2026-02-23 19:22:46 +0530

    Expense filr added

---
+++ Expense.java
@@ -0,0 +1,18 @@
+package ExpenseTracker;
+
+class Expense {
+    String category;
+    double amount;
+    String date;
+
+    Expense(String category, double amount, String date) {
+        this.category = category;
+        this.amount = amount;
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return category + "," + amount + "," + date;
+    }
+}
